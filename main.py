b5381fd338243dbea437d1882e53f99fba7acf90
Eden Iyanda <edeniyanda@gmail.com>
2024-05-28 10:02:00 +0100

    Update

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from time import sleep
 APPNAME = "BFC"
 
 
+
+
 def check_battery():
     battery = psutil.sensors_battery()
     if battery is not None:
@@ -24,20 +26,27 @@ def notify(title, message):
         title=title,
         message=message,
         app_name=APPNAME,
-        timeout=20
+        timeout=1000
     )
 
 def main():
+    current_percent = 0
     print(F"Welcome to {APPNAME}\n\n")
     sleep(0.5) 
     for i in trange(100):
         sleep(.02)
     sleep(1)
     print(f"\n{APPNAME} now working in background, and will notify you once your PC is fully charged")
-    print("DO NOT close this window, you can minimize the window")
+    print("DO NOT close this window, you can minimize the window\n")
     while True:
         try:
             percent = check_battery()[1]
+            if current_percent < percent:
+                print(f"Your Battery is currently at {'.'*50}{percent}%\n")
+            elif current_percent > percent and current_percent != 0:
+                print(f"WARNING: Your PC is discharging in a funny way\n\nYour Battery is currently at {'.'*50}{percent}%\n")
+            current_percent = percent
+
             if check_battery()[0]:
                 notify("Battery Fully Charged", "Your battery is fully charged, you can unplug it!")
                 print("\n\nYour PC is fully charged, you can unplug it now")
